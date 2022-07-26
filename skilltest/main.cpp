bea0149e79247610d61c4617daa5013e905b02d6
Simon wagura <simowagura9@gmail.com>
2022-07-26 22:07:07 +0300

    Add project files.

---
+++ skilltest/main.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+	int a = 10;
+	int b = 20;
+	int c = a + b;
+	cout << c << "\n";
+	int numbers[5] = { 1,2,3,4,5 };
+	for (int i = 0; i < 5; i++) {
+		cout << numbers[i] <<"\n";
+	}
+
+}
